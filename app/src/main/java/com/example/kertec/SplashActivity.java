d576d36a71d9a45f8e326124dfb44a683a25fa84
CarlosR3y <al02947993@tecmilenio.mx>
2023-03-30 23:17:09 -0600

    Nuevos archivos se agregan

---
+++ app/src/main/java/com/example/kertec/SplashActivity.java
@@ -0,0 +1,31 @@
+package com.example.kertec;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+
+
+    private static int SPLASH_TIME_OUT = 400; // tiempo en milisegundos
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, SlideActivity.class);
+                startActivity(intent);
+                finish(); // cerrar esta actividad
+            }
+        }, SPLASH_TIME_OUT);
+
+    }
+}
