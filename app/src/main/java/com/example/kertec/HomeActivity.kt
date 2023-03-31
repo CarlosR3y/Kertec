9a52c319f248673b97a11910f38de91e60454454
CarlosR3y <al02947993@tecmilenio.mx>
2023-03-30 23:14:07 -0600

    Nuevos archivos se agregan

--- app/src/main/java/com/example/kertec/HomeActivity.kt
+++ app/src/main/java/com/example/kertec/HomeActivity.kt
@@ -1,16 +1,23 @@
 package com.example.kertec
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
 import com.example.kertec.R
+import kotlinx.android.synthetic.main.activity_home.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
+        fab.setOnClickListener{
+            val intent = Intent(this, OrdenActivity::class.java)
+            startActivity(intent)
+        }
 
     }
 }
